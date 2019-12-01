00d2b8d2ada4727fcbb0741ed04c4235129ecd23
Tralv <56794703+Tralv@users.noreply.github.com>
2019-12-01 13:46:31 -0600

    Update appp.js

--- problema_1 y 2/dsfghdfh/appp.js
+++ problema_1 y 2/dsfghdfh/appp.js
@@ -1,6 +1,6 @@
 var val = document.getElementById('titulo');
  val.innerHTML = '<b>Hola</b> Mundo';
 
-document.querySelector('class="row"').style.padding = '20px'
-
+document.querySelector(".row"').style.padding = '20px';
+document.querySelector(".roredw"').style.color = 'red';
 console.log(val);
